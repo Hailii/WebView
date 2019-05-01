ec74e938c7d4457f8aad0d890d25c78beef304c1
unknown <80400081@qq.com>
2019-05-01 15:32:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity{
+
+    private String uriHome = "http://www.baidu.com";
+    private String url;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        Intent intent = getIntent();
+        Uri data = intent.getData();
+        if (data == null) {
+            url = "http://www.baidu.com";
+            data = Uri.parse(url);
+        }
+        webView.loadUrl(data.toString());
+    }
+}
